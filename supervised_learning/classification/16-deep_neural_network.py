94d894964c7dbbee83b93cdd32c213896255c3ed
kevin10v <vokakevin@icloud.com>
2026-01-23 12:42:52 +0100

    Task 16: DeepNeuralNetwork

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python3
+'''
+Module that defines a deep neural network
+performing binary classification
+'''
 import numpy as np
 
-class DeepNeuralNetwork:
-    def __init__(self,nx,layers):
-        if not isinstance(nx,int):
+
+class DeepNeuralNetwork():
+    '''Class that defines a deep neural network'''
+    def __init__(self, nx, layers):
+        '''Class constructur'''
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
-        if nx<1:
+        if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers,list)or len(layers)==0:
+        if type(layers) is not list:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(n,int)and n>0 for n in layers):
+        if len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        self.L=len(layers)
-        self.cache={}
-        self.weights={}
-        for layer in range(1,self.L+1):
-            if layer==1:
-                self.weights["W%d"%layer]=np.random.randn(layers[layer-1],nx)*np.sqrt(2./nx)
-            else:
-                self.weights["W%d"%layer]=np.random.randn(layers[layer-1],layers[layer-2])*np.sqrt(2./layers[layer-2])
-            self.weights["b%d"%layer]=np.zeros((layers[layer-1],1))
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        for x in range(self.L):
+            if layers[x] < 1 or type(layers[x]) is not int:
+                raise TypeError("layers must be a list of positive integers")
+            self.weights["b" + str(x + 1)] = np.zeros((layers[x], 1))
+            if x == 0:
+                He_et_0 = np.random.randn(layers[x], nx) * np.sqrt(2 / nx)
+                self.weights["W" + str(x + 1)] = He_et_0
+            if x > 0:
+                He_et_1 = np.random.randn(layers[x], layers[x - 1])
+                He_et_2 = np.sqrt(2 / layers[x - 1])
+                He_et_3 = He_et_1 * He_et_2
+                self.weights["W" + str(x + 1)] = He_et_3
--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -1,52 +1,50 @@
 #!/usr/bin/env python3
-"""Defines DeepNeuralNetwork performing binary classification"""
-
-
+'''
+Module that defines a deep neural network
+performing binary classification
+'''
 import numpy as np
 
 
-class DeepNeuralNetwork:
-    """Deep neural network class"""
-
+class DeepNeuralNetwork():
+    '''Class that defines a deep neural network'''
     def __init__(self, nx, layers):
-        """Constructor"""
-        if not isinstance(nx, int):
+        '''Class constructur'''
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list):
+        if type(layers) is not list:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(n, int) and n > 0 for n in layers):
+        if len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
-
-        for layer in range(1, self.__L + 1):
-            if layer == 1:
-                self.__weights["W%d" % layer] = (
-                    np.random.randn(layers[layer - 1], nx) *
-                    np.sqrt(2. / nx)
-                )
-            else:
-                self.__weights["W%d" % layer] = (
-                    np.random.randn(layers[layer - 1], layers[layer - 2]) *
-                    np.sqrt(2. / layers[layer - 2])
-                )
-            self.__weights["b%d" % layer] = np.zeros((layers[layer - 1], 1))
+        for x in range(self.L):
+            if layers[x] < 1 or type(layers[x]) is not int:
+                raise TypeError("layers must be a list of positive integers")
+            self.__weights["b" + str(x + 1)] = np.zeros((layers[x], 1))
+            if x == 0:
+                He_et_0 = np.random.randn(layers[x], nx) * np.sqrt(2 / nx)
+                self.__weights["W" + str(x + 1)] = He_et_0
+            if x > 0:
+                He_et_1 = np.random.randn(layers[x], layers[x - 1])
+                He_et_2 = np.sqrt(2 / layers[x - 1])
+                He_et_3 = He_et_1 * He_et_2
+                self.__weights["W" + str(x + 1)] = He_et_3
 
     @property
     def L(self):
-        """L getter"""
+        '''L getter function'''
         return self.__L
 
     @property
-    def cache(self):
-        """cache getter"""
-        return self.__cache
+    def weights(self):
+        '''weight getter function'''
+        return self.__weights
 
     @property
-    def weights(self):
-        """weights getter"""
-        return self.__weights
+    def cache(self):
+        '''cache getter function'''
+        return self.__cache
