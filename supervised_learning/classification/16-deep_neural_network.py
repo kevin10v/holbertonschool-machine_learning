baceea77e99c86dbd045231859469b4faa935227
kevin10v <vokakevin@icloud.com>
2026-01-23 12:26:54 +0100

    Task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,31 +1,22 @@
 #!/usr/bin/env python3
-"""Defines a deep neural network performing binary classification"""
 import numpy as np
 
-
 class DeepNeuralNetwork:
-    """Deep neural network class"""
-
-    def __init__(self, nx, layers):
-        """Initialize the deep neural network"""
-        if not isinstance(nx, int):
+    def __init__(self,nx,layers):
+        if not isinstance(nx,int):
             raise TypeError("nx must be an integer")
-        if nx < 1:
+        if nx<1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0:
+        if not isinstance(layers,list)or len(layers)==0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(n, int) and n > 0 for n in layers):
+        if not all(isinstance(n,int)and n>0 for n in layers):
             raise TypeError("layers must be a list of positive integers")
-
-        self.L = len(layers)
-        self.cache = {}
-        self.weights = {}
-
-        for layer in range(1, self.L + 1):
-            if layer == 1:
-                self.weights["W{}".format(layer)] = np.random.randn(
-                    layers[layer - 1], nx) * np.sqrt(2 / nx)
+        self.L=len(layers)
+        self.cache={}
+        self.weights={}
+        for layer in range(1,self.L+1):
+            if layer==1:
+                self.weights["W%d"%layer]=np.random.randn(layers[layer-1],nx)*np.sqrt(2./nx)
             else:
-                self.weights["W{}".format(layer)] = np.random.randn(
-                    layers[layer - 1], layers[layer - 2]) * np.sqrt(2 / layers[layer - 2])
-            self.weights["b{}".format(layer)] = np.zeros((layers[layer - 1], 1))
+                self.weights["W%d"%layer]=np.random.randn(layers[layer-1],layers[layer-2])*np.sqrt(2./layers[layer-2])
+            self.weights["b%d"%layer]=np.zeros((layers[layer-1],1))
