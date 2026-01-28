8ae21e289c17eaf74bea8295d3c00fbf801b089a
kevin10v <vokakevin@icloud.com>
2026-01-28 15:58:24 +0100

    Add task 0: Sequential model with L2 regularization and dropout

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Module that builds a neural network with Keras"""
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Builds a neural network with the Keras library
+    
+    Args:
+        nx: number of input features to the network
+        layers: list containing the number of nodes in each layer
+        activations: list containing the activation functions for each layer
+        lambtha: L2 regularization parameter
+        keep_prob: probability that a node will be kept for dropout
+    
+    Returns:
+        the keras model
+    """
+    model = K.Sequential()
+    
+    for i in range(len(layers)):
+        if i == 0:
+            # First layer needs input_shape
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=K.regularizers.L2(lambtha),
+                input_shape=(nx,)
+            ))
+        else:
+            # Subsequent layers
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=K.regularizers.L2(lambtha)
+            ))
+        
+        # Add Dropout after each layer except the last one
+        if i < len(layers) - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
+    
+    return model
