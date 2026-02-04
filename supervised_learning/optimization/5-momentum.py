eac3559144b928f517ec1b82ad51f4b3026592f2
kevin10v <vokakevin@icloud.com>
2026-02-04 17:50:59 +0100

    Add task 5: Gradient descent with momentum

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module for gradient descent with momentum optimization."""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    Updates a variable using gradient descent with momentum optimization.
+
+    Args:
+        alpha: learning rate
+        beta1: momentum weight
+        var: numpy.ndarray containing the variable to be updated
+        grad: numpy.ndarray containing the gradient of var
+        v: previous first moment of var
+
+    Returns:
+        The updated variable and the new moment, respectively
+    """
+    v = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * v
+    return var, v
