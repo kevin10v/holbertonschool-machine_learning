34a81bd3eb052cc82c9e9870061d2ae2a67b48b0
kevin10v <vokakevin@icloud.com>
2026-02-04 17:37:28 +0100

    Add task 2: Shuffle data

---
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Module for shuffling data."""
+
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """
+    Shuffles the data points in two matrices the same way.
+
+    Args:
+        X: first numpy.ndarray of shape (m, nx) to shuffle
+           m is the number of data points
+           nx is the number of features in X
+        Y: second numpy.ndarray of shape (m, ny) to shuffle
+           m is the same number of data points as in X
+           ny is the number of features in Y
+
+    Returns:
+        The shuffled X and Y matrices
+    """
+    m = X.shape[0]
+    permutation = np.random.permutation(m)
+    return X[permutation], Y[permutation]
