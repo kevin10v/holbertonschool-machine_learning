9973f1e45ce53fd7ab5b91bc2e82b35e437e6dfd
kevin10v <vokakevin@icloud.com>
2026-02-05 13:01:56 +0100

    Add task 3: Create layer with L2 regularization in TensorFlow

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module for creating a layer with L2 regularization."""
+
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    Creates a neural network layer with L2 regularization.
+
+    Args:
+        prev: tensor containing the output of the previous layer
+        n: number of nodes the new layer should contain
+        activation: activation function for the layer
+        lambtha: L2 regularization parameter
+
+    Returns:
+        Output of the new layer
+    """
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode=('fan_avg')
+    )
+
+    regularizer = tf.keras.regularizers.L2(lambtha)
+
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer,
+        kernel_regularizer=regularizer
+    )
+
+    return layer(prev)
